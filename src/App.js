ea129c3773b2c3cd5c172cbc2c43a577e4f6d72f
Dylan Grimes Larkin <dylangrimeslarkin@gmail.com>
2025-04-27 15:44:57 +0100

    Zoom changed to 14

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ function App() {
         <GoogleMap
           mapContainerStyle={containerStyle}
           center={OPO}
-          zoom={12}
+          zoom={14}
         >
           <Marker
             position={OPO}
