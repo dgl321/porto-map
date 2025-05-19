62ba7dca8610b4df9f6ec4dd6676fd7eb5f5d17a
dgl321 <dylangrimeslarkin@gmail.com>
2025-05-19 16:11:25 +0100

    Remove Open Graph and Twitter meta tags from App.js and update headers in _headers file for improved configuration

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import { Helmet } from 'react-helmet';
 import MainMap from './MainMap';
 import Places from './Places';
 import Food from './Food';
@@ -77,25 +76,6 @@ function App() {
 
   return (
     <Router>
-      <Helmet>
-        <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://porto-map.netlify.app/" />
-        <meta property="og:title" content="Porto Map" />
-        <meta property="og:description" content="Interactive map of Porto" />
-        <meta property="og:image" content="https://porto-map.netlify.app/porto.webp" />
-        <meta property="og:image:type" content="image/webp" />
-        <meta property="og:image:width" content="1200" />
-        <meta property="og:image:height" content="630" />
-        <meta property="og:image:alt" content="Porto Map" />
-        <meta property="og:site_name" content="Porto Map" />
-        <meta property="og:logo" content="https://porto-map.netlify.app/nata.jpg" />
-
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:url" content="https://porto-map.netlify.app/" />
-        <meta name="twitter:title" content="Porto Map" />
-        <meta name="twitter:description" content="Interactive map of Porto" />
-        <meta name="twitter:image" content="https://porto-map.netlify.app/porto.webp" />
-      </Helmet>
       <ErrorBoundary>
         <Routes>
           <Route path="/" element={<MainMap />} />
