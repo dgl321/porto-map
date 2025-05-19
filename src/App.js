0c170642f034d6f00b36ee0c12f5674b01efe1c8
dgl321 <dylangrimeslarkin@gmail.com>
2025-05-19 13:33:28 +0100

    feat: add React Helmet for dynamic meta tags

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Helmet } from 'react-helmet';
 import MainMap from './MainMap';
 import Places from './Places';
 import Food from './Food';
@@ -8,6 +9,23 @@ import Essentials from './Essentials';
 function App() {
   return (
     <Router>
+      <Helmet>
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://porto-map.netlify.app/" />
+        <meta property="og:title" content="Porto Map" />
+        <meta property="og:description" content="Interactive map of Porto" />
+        <meta property="og:image" content="https://porto-map.netlify.app/porto.jpeg" />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
+        <meta property="og:image:alt" content="Porto Map" />
+        <meta property="og:site_name" content="Porto Map" />
+        
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:url" content="https://porto-map.netlify.app/" />
+        <meta name="twitter:title" content="Porto Map" />
+        <meta name="twitter:description" content="Interactive map of Porto" />
+        <meta name="twitter:image" content="https://porto-map.netlify.app/porto.jpeg" />
+      </Helmet>
       <Routes>
         <Route path="/" element={<MainMap />} />
         <Route path="/places" element={<Places />} />
